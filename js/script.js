e612cd9f783996ea9d2b99cf41c0c1ba99a67cb5
Anjali <menon.anjali11@gmail.com>
2021-03-16 11:53:24 +1300

    refactor code and add onkey up event

--- js/script.js
+++ js/script.js
@@ -1,92 +1,92 @@
 window.onload = function () {
-	
-
 	submit = () => {
-		let fname = document.getElementById("fname").value;
-		let lname = document.getElementById("lname").value;
-		let email = document.getElementById("email").value;
-		let pwd = document.getElementById("pwd").value;
-
-		if (!fname && !lname && !email && !pwd) {
-			document.getElementById("fname").style.border =
-				"2px solid hsl(0,100%,74%)";
-
-			document.getElementById("lname").style.border =
-				"2px solid hsl(0,100%,74%)";
-			document.getElementById("email").style.border =
-				"2px solid hsl(0,100%,74%)";
-			document.getElementById("pwd").style.border = "2px solid hsl(0,100%,74%)";
-
-			let icon = document.getElementsByClassName("err-icon");
-			for (var i = 0; i < icon.length; i++) {
-				icon[i].style.visibility = "visible";
-				icon[i].style.float = "right";
-				icon[i].style.marginRight = "21px";
-				icon[i].style.marginTop = "-56px";
-				icon[i].style.marginBottom = "0";
-				icon[i].style.position = "relative";
-				icon[i].style.zIndex = "2";
-			}
-
-			document.getElementById("err-fname").innerText =
-				"First Name cannot be empty";
-
-			document.getElementById("err-lname").innerText =
-				"Last Name cannot be empty";
-			document.getElementById("err-email").innerText = "Email cannot be empty";
-			document.getElementById("err-pwd").innerText = "Password cannot be empty";
-		}
-		if (!fname || fname === "") {
-			document.getElementById("fname").style.border =
-				"2px solid hsl(0,100%,74%)";
-
-			document.getElementById("err-fname").innerText =
-				"First Name cannot be empty";
-			document.getElementById("err-fname").style.visibility = "visible";
-			document.getElementById("err-fname").style.color = "hsl(0,100%,74%)";
-
-			document.getElementById("err-fname").style.fontSize = "10px";
-			document.getElementById("err-fname").style.float = "right";
-		}
-		if (!lname || lname === "") {
-			document.getElementById("lname").style.border =
-				"2px solid hsl(0,100%,74%)";
-
-			document.getElementById("err-lname").innerText =
-				"Last Name cannot be empty";
-			document.getElementById("err-lname").style.visibility = "visible";
-			document.getElementById("err-lname").style.color = "hsl(0,100%,74%)";
-
-			document.getElementById("err-lname").style.fontSize = "10px";
-			document.getElementById("err-lname").style.float = "right";
-		}
-		if (validateEmail(email) || email === "") {
-			document.getElementById("email").style.border =
-				"2px solid hsl(0,100%,74%)";
-
-			document.getElementById("err-email").innerText =
-				"Looks like this is not an email";
-			document.getElementById("err-email").style.visibility = "visible";
-			document.getElementById("err-email").style.color = "hsl(0,100%,74%)";
-
-			document.getElementById("err-email").style.fontSize = "10px";
-
-			document.getElementById("err-email").style.float = "right";
-		}
-		if (!pwd || pwd === "") {
-			document.getElementById("pwd").style.border = "2px solid hsl(0,100%,74%)";
-
-			document.getElementById("err-pwd").innerText =
-				"Password cannot be empty";
-			document.getElementById("err-pwd").style.visibility = "visible";
-			document.getElementById("err-pwd").style.color = "hsl(0,100%,74%)";
-
-			document.getElementById("err-pwd").style.fontSize = "10px";
-			document.getElementById("err-pwd").style.float = "right";
-		}
+		let fname = document.getElementById("fname");
+		let lname = document.getElementById("lname");
+		let email = document.getElementById("email");
+		let pwd = document.getElementById("pwd");
+		validateInput(fname.getAttribute("id"));
+		validateInput(lname.getAttribute("id"));
+		validateInput(pwd.getAttribute("id"));
+		validateEmail(email.getAttribute("id"));
 	};
 };
-validateEmail = (email) => {
-	let regex = /\S+@\S+\.\S+/;
-	return regex.test(email);
+validateEmail = (id) => {
+	console.log(id);
+	let email = document.getElementById(id).value;
+	let regex = /^[a-zA-z0-9._]+@[a-zA-z0-9._]+\.[a-zA-z0-9]{2,4}$/;
+
+	if (!regex.test(email)) {
+		document.getElementById("email").style.border = "2px solid hsl(0,100%,74%)";
+
+		document.getElementById("err-email").innerText =
+			"Looks like this is not an email";
+		document.getElementById("err-email").style.visibility = "visible";
+		document.getElementById("err-email").style.color = "hsl(0,100%,74%)";
+
+		document.getElementById("err-email").style.fontSize = "10px";
+
+		document.getElementById("err-email").style.float = "right";
+	} else {
+		document.getElementById(`icon-${id}`).style.visibility = "hidden";
+		document.getElementById(id).style.borderColor = "hsl(154, 59%, 51%)";
+
+		document.getElementById(`err-${id}`).style.visibility = "hidden";
+	}
+};
+validateInput = (id) => {
+	let input = document.getElementById(id).value;
+	if (!input || input === "") {
+		document.getElementById(id).style.border = "2px solid hsl(0,100%,74%)";
+		switch (id) {
+			case "fname":
+				document.getElementById(`err-${id}`).innerText =
+					"First Name cannot be empty";
+
+				break;
+			case "lname":
+				document.getElementById(`err-${id}`).innerText =
+					"Last Name cannot be empty";
+				break;
+			case "pwd":
+				document.getElementById(`err-${id}`).innerText =
+					"Password cannot be empty";
+				break;
+		}
+
+		document.getElementById(`err-${id}`).style.visibility = "visible";
+		document.getElementById(`err-${id}`).style.color = "hsl(0,100%,74%)";
+
+		document.getElementById(`err-${id}`).style.fontSize = "10px";
+		document.getElementById(`err-${id}`).style.float = "right";
+	} else {
+		document.getElementById(id).style.borderColor = "hsl(154, 59%, 51%)";
+
+		document.getElementById(`err-${id}`).style.visibility = "hidden";
+
+		switch (id) {
+			case "fname":
+				document.getElementById(`icon-${id}`).style.visibility = "hidden";
+				break;
+			case "lname":
+				document.getElementById(`icon-${id}`).style.visibility = "hidden";
+				break;
+
+			case "password":
+				document.getElementById(`icon-${id}`).style.visibility = "hidden";
+				break;
+		}
+	}
+};
+
+handleInputChange = (e) => {
+	let id = e.id;
+	if (e.value) {
+		document.getElementById(id).value = e.value;
+		validateInput(id);
+	}
+};
+handleEmailInput = () => {
+	let id = e.id;
+
+	validateEmail(id);
 };
